db7552f032a96584430ed2adb7a414a58e68f231
guedesvv <joaovguedes22@gmail.com>
2023-06-01 16:12:28 -0300

    subindo rep orientação a objetos

---
+++ OrientaçãoObjetos/Classes.cs
@@ -0,0 +1,18 @@
+﻿class Carro
+{
+
+    public string Nome { get; set; }
+
+    public int Ano { get; set; }
+
+    public string Cor { get; set; }
+
+    public string DescricaoCompleta => $"Nome do carro: {Nome}\nAno: {Ano}\nCor: {Cor}\n";
+
+    public void ExibirInfos() {
+
+        Console.WriteLine(DescricaoCompleta);
+    
+    }
+
+}
---
+++ OrientaçãoObjetos/Program.cs
@@ -0,0 +1,18 @@
+﻿Carro carro1 = new Carro();
+Carro carro2 = new Carro();
+
+
+carro1.Nome = "Corsa";
+carro1.Ano = 2005;
+carro1.Cor = "vermelho";
+
+
+carro2.Nome = "Passat";
+carro2.Ano = 1980;
+carro2.Cor = "abacate";
+
+
+carro1.ExibirInfos();
+carro2.ExibirInfos();
+
+
